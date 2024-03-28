627049069d033edf495de920f217ce516c243785
aatchison <arron.atchison@gmail.com>
2024-03-28 16:24:14 -0700

    stop specifying file in tarball

--- src/idpbuilder/install.sh
+++ src/idpbuilder/install.sh
@@ -6,8 +6,8 @@ releaseVersion=${releaseVersion:-undefined}
 echo "Installing idpbuilder from release $releaseVersion'"
 
 # For Kubectl AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -L https://github.com/cnoe-io/idpbuilder/releases/download/v0.3.0/idpbuilder-linux-${releaseVersion}.tar.gz | tar xz idpbuilder
+[ $(uname -m) = x86_64 ] && curl -L https://github.com/cnoe-io/idpbuilder/releases/download/v0.3.0/idpbuilder-linux-${releaseVersion}.tar.gz | tar xz
 # For Kubectl ARM64
-[ $(uname -m) = aarch64 ] && curl -L https://github.com/cnoe-io/idpbuilder/releases/download/v0.3.0/idpbuilder-linux-${releaseVersion}.tar.gz | tar xz idpbuilder
+[ $(uname -m) = aarch64 ] && curl -L https://github.com/cnoe-io/idpbuilder/releases/download/v0.3.0/idpbuilder-linux-${releaseVersion}.tar.gz | tar xz
 chmod +x ./idpbuilder
 sudo mv ./idpbuilder /usr/local/bin/idpbuilder
