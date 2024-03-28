164c01d268d4066d6cfc9e5a4ee79247b6fecc04
aatchison <arron.atchison@gmail.com>
2024-03-28 16:13:47 -0700

    init

---
+++ src/idpbuilder/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+releaseVersion=${releaseVersion:-undefined}
+
+echo "Installing idpbuilder from release $releaseVersion'"
+
+# For Kubectl AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -L https://github.com/cnoe-io/idpbuilder/releases/download/v0.3.0/idpbuilder-linux-${releaseVersion}.tar.gz | tar xz idpbuilder
+# For Kubectl ARM64
+[ $(uname -m) = aarch64 ] && curl -L https://github.com/cnoe-io/idpbuilder/releases/download/v0.3.0/idpbuilder-linux-${releaseVersion}.tar.gz | tar xz idpbuilder
+chmod +x ./idpbuilder
+sudo mv ./idpbuilder /usr/local/bin/idpbuilder
