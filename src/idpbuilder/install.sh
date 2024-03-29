788f1ca01405c1da73a989b8f94bf45d56b4c999
Arron Atchison <arron.atchison@gmail.com>
2024-03-28 17:10:05 -0700

    init (#2)
    * init
    
    * enable tests for idpbuilder
    
    * add idpbuilder tests
    
    * stop specifying file in tarball
    
    * change base container
    
    * revert base container, add curl feature
    
    * revert, install curl
    
    * install curl
    
    * sudo?
    
    * revert curl install
    
    * fix installation and tests
    
    * fix test scenario
    
    * remove color command from test
    
    * fix scenario and tests

---
+++ src/idpbuilder/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+apt-get update && apt-get install -y curl 
+
+
+releaseVersion=${RELEASEVERSION:-undefined}
+
+echo "Installing idpbuilder from release $releaseVersion'"
+
+# For Kubectl AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -L https://github.com/cnoe-io/idpbuilder/releases/download/$releaseVersion/idpbuilder-linux-amd64.tar.gz | tar xz
+# For Kubectl ARM64
+[ $(uname -m) = aarch64 ] && curl -L https://github.com/cnoe-io/idpbuilder/releases/download/$releaseVersion/idpbuilder-linux-amd64.tar.gz | tar xz
+chmod +x ./idpbuilder
+mv ./idpbuilder /usr/local/bin/idpbuilder
