95d46d16ea89a578046e303db30bd3742dc9e645
aatchison <arron.atchison@gmail.com>
2024-04-01 12:49:19 -0700

    upstream package release file is names x86_64, not amd64

--- src/cnoe-cli/install.sh
+++ src/cnoe-cli/install.sh
@@ -9,7 +9,7 @@ releaseVersion=${RELEASEVERSION:-undefined}
 echo "Installing cnoe-cli from release $releaseVersion'"
 
 # For cnoe-cli AMD64 / x86_64
-# [ $(uname -m) = x86_64 ] && curl -L https://github.com/cnoe-io/cnoe-cli/releases/download/$releaseVersion/cnoe_Linux_amd64.tar.gz | tar xz cnoe
+[ $(uname -m) = x86_64 ] && curl -L https://github.com/cnoe-io/cnoe-cli/releases/download/$releaseVersion/cnoe_Linux_x86_64.tar.gz | tar xz cnoe
 # For cnoe-cli ARM64
 [ $(uname -m) = aarch64 ] && curl -L https://github.com/cnoe-io/cnoe-cli/releases/download/$releaseVersion/cnoe_Linux_arm64.tar.gz | tar xz cnoe
 chmod +x ./cnoe
